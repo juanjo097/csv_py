99f154c68475bfdd3d70aa599a502dea3c53cc4b
juanjo097 <rodriguez.rangel97@gmail.com>
2019-10-26 00:07:45 -0500

    adecuaciones para generar create tables

--- main.py
+++ main.py
@@ -6,73 +6,109 @@ import sys
 import os
 import numpy as np
 
+init(autoreset=True)
 
-def main():
-    init()
-    print(Fore.GREEN + "Escribiendo archivo sql...")
-    start_time = time.time()
+def create_t(data, f_c, name_file):
+    try:
+        t_n = "CREATE TABLE " + "`" + name_file + "`" + " (\n"
+        f_c.write(t_n)
 
-    #reading file from path
-    path = sys.argv[1]
-    # dataframe dirty
-    dfd = pd.read_csv(path)
-    # dataframe with all columns emptys cleaned
-    df = dfd.dropna(axis='columns', how='all')
-    df = dfd.replace(np.nan, 'NULL', regex=True)
+        for i in range(0, len(data)):
+            if 'id' in data[i]:
+                f_c.write("\t`")
+                f_c.write(data[i].lower())
+                f_c.write("` ")
 
-    name_file = path.split('\\')
-    name_file = name_file[- 1][:-4]
-    o_f = os.path.join("generated_files/2010/", name_file + ".sql")
-    # output file with utf-8 encode
-    f = codecs.open(o_f, 'w+', 'utf-8')
+                if i == len(data) - 1:
+                    f_c.write("INT UNSIGNED DEFAULT NULL\n")
+                else:
+                    f_c.write("INT UNSIGNED DEFAULT NULL,\n")
 
-    for i in range(0, len(df.values)):
+            else:
+                f_c.write("\t`")
+                f_c.write(data[i].lower())
+                f_c.write("` ")
 
-        f.write("INSERT INTO ")
-        f.write(name_file)
-        f.write("( ")
+                if i == len(data) - 1:
+                    f_c.write("VARCHAR(255) DEFAULT NULL\n")
+                else:
+                    f_c.write("VARCHAR(255) DEFAULT NULL,\n")
+        f_c.write(");")
+    except e:
+        print(Fore.RED + "Error al generar archivo CREATE")
+
+def insert_t():
+    try:
+        print(Fore.GREEN + "Escribiendo archivo sql...")
+        start_time = time.time()
+
+        #reading file from path
+        path = sys.argv[1]
+        # dataframe dirty
+        dfd = pd.read_csv(path)
+        # dataframe with all columns emptys cleaned
+        df = dfd.dropna(axis='columns', how='all')
+        df = dfd.replace(np.nan, 0, regex=True)
+        name_file = path.split('\\')
+        name_file = name_file[- 1][:-4]
+        o_f_i = os.path.join("generated_files/2010/", name_file + ".sql")
+        o_f_c = os.path.join("generated_files/2010/", "create_table_" + name_file + ".sql")
+        # output file with utf-8 encode
+        f_i = codecs.open(o_f_i, 'w+', 'utf-8')
+        f_c = codecs.open(o_f_c, 'w+', 'utf-8')
+
+        f_i.write("INSERT INTO ")
+        f_i.write(name_file)
+        f_i.write("(")
 
         for h in range(0, len(df.columns.values)):
-            f.write(df.columns.values[h])
+            if h == 0:
+                f_i.write("\n")
+                df.columns.values[0] = "id_" + name_file
+
+            f_i.write(df.columns.values[h].lower())
+
             if h == len(df.columns.values) - 1:
-                f.write(")")
+                f_i.write(") VALUES\n")
             else:
-                f.write(",")
+                f_i.write(",\n")
 
-        f.write("VALUES( ")
+        create_t(df.columns.values,f_c,name_file)
 
-        for k in range(0, len(df.values[i])):
-            if(isinstance(df.values[i][k], int)):
-                f.write(str(df.values[i][k]))
-                if k == len(df.columns.values) - 1:
-                    f.write("")
-                else:
-                    f.write(",")
+        for i in range(0, len(df.values)):
+            f_i.write("(")
+            for k in range(0, len(df.values[i])):
+                if(isinstance(df.values[i][k], int)):
+                    f_i.write(str(df.values[i][k]))
+                    if k != len(df.columns.values) - 1:
+                        f_i.write(",")
 
-            if(isinstance(df.values[i][k], str)):
-                f.write("\"")
-                f.write(df.values[i][k])
-                f.write("\"")
-                if k == len(df.columns.values) - 1:
-                    f.write("")
-                else:
-                    f.write(",")
+                if(isinstance(df.values[i][k], str)):
+                    f_i.write("\"")
+                    f_i.write(df.values[i][k].replace("\"", " "))
+                    f_i.write("\"")
+                    if k != len(df.columns.values) - 1:
+                        f_i.write(",")
 
-            if(isinstance(df.values[i][k], float)):
-                f.write(str(df.values[i][k]))
-                if k == len(df.columns.values) - 1:
-                    f.write("")
-                else:
-                    f.write(",")
+                if(isinstance(df.values[i][k], float)):
+                    f_i.write(str(df.values[i][k]))
+                    if k != len(df.columns.values) - 1:
+                        f_i.write(",")
 
-        if [i] == len(df.columns.values) - 1:
-            f.write("\n")
-        else:
-            f.write(");\n")
-    f.close()
+            if i == len(df.values) - 1:
+                f_i.write(");")
+            else:
+                f_i.write("),\n")
 
-    print("Archivo de salida: " + name_file + ".sql")
-    print("Parseado terminado : %s" % (time.time() - start_time))
+        f_i.close()
+
+        print(Fore.YELLOW + "Archivo de salida: " + name_file + ".sql")
+        print(Fore.GREEN +"Parseado terminado : %s" % (time.time() - start_time))
+    except:
+        print(Fore.RED + "Error al generar archivo de INSERT")
+
+def main():
+    insert_t()
 
 if __name__ == "__main__":
     main()
