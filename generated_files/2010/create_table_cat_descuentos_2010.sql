83b016388e6afe49be904e770ef9c436181d433b
juanjo097 <rodriguez.rangel97@gmail.com>
2019-10-30 11:14:24 -0600

    se remueven los csv's de pruebas

--- generated_files/2010/create_table_cat_descuentos_2010.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `cat_descuentos_2010` (
-	`id_cat_descuentos` INT UNSIGNED DEFAULT NULL,
-	`tipo` VARCHAR(255) DEFAULT NULL,
-	`nombre` TEXT DEFAULT NULL,
-	`importe` VARCHAR(255) DEFAULT NULL
-);
--- generated_files/2011/create_table_capitulos_2011.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `capitulos_2011` (
-	`id_capitulos` INT UNSIGNED DEFAULT NULL,
-	`id_presupuesto` INT UNSIGNED DEFAULT NULL,
-	`clave` VARCHAR(255) DEFAULT NULL,
-	`descripcion` VARCHAR(255) DEFAULT NULL,
-	`importe` VARCHAR(255) DEFAULT NULL,
-	`id_contrato_ampliacion` INT UNSIGNED DEFAULT NULL
-);
